0a706cb8ff89e9bfcfb68d09a8d4246257b263e5
LuissMello <LuissMello@users.noreply.github.com>
2025-07-31 15:43:41 +0000

    deploy: deff7f091d7b7940782cef445f0afa10c0032920

--- src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'https://eafctracker-cvadcceuerbgegdj.brazilsouth-01.azurewebsites.net/api/Matches',
-});
-
-export default api;
